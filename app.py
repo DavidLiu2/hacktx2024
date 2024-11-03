bf19a7684387ec90516eb12105277cd439237031
DavidLiu2 <dliu5545@gmail.com>
2024-11-03 12:27:44 -0600

    revert

--- app.py
+++ app.py
@@ -54,6 +54,4 @@ def predict():
 
 # Run the Flask app
 if __name__ == '__main__':
-    # Avoid setting a specific host and port in app.py
-    app.run(host="127.0.0.1", port=5000)
-
+    app.run(debug=True)
